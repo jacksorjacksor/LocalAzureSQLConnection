732d95a2427d8b82005b644a42d32820cfe0255f
Richard Jackson <jacksorjacksor@pm.me>
2022-12-24 21:10:18 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+
+try
+{
+    SqlConnectionStringBuilder builder = new()
+    {
+        DataSource = "localhost",
+        UserID = "sa",
+        Password = "testpassword0?",
+        InitialCatalog = "mylocaldbproject01",
+        Encrypt = false
+    };
+
+    using SqlConnection connection = new(builder.ConnectionString);
+
+    String sql = "SELECT * FROM  [mylocaldbproject01].[dbo].[mylocaltable01]";
+
+    using SqlCommand command = new(sql, connection);
+    connection.Open();
+    using SqlDataReader reader = command.ExecuteReader();
+    Console.WriteLine("ID\tName");
+    while (reader.Read())
+    {
+        Console.WriteLine($"{reader.GetInt32(0)}\t{reader.GetString(1)}");
+    }
+}
+catch (SqlException e)
+{
+    Console.WriteLine(e.ToString());
+}
+Console.ReadLine();
